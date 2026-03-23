da844e8a6872fcd958e5fcaf68953dd5875d6e98
trungkien963 <trungkien963@gmail.com>
2026-03-23 16:53:47 +0700

    feat: add animated Mantu logo to hero section

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  targetOrders = 845;
+  targetDonation = 24500000;
+  
+  displayedOrders = 0;
+  displayedDonation = 0;
+
+  ngOnInit() {
+    this.animateCounter('displayedOrders', this.targetOrders, 1500);
+    this.animateCounter('displayedDonation', this.targetDonation, 2000);
+  }
+
+  animateCounter(prop: 'displayedOrders' | 'displayedDonation', target: number, duration: number) {
+    const steps = 40;
+    const stepTime = Math.abs(Math.floor(duration / steps));
+    let currentStep = 0;
+    
+    const timer = setInterval(() => {
+      currentStep++;
+      const progress = currentStep / steps;
+      // Ease out cubic
+      const easeOut = 1 - Math.pow(1 - progress, 3);
+      this[prop] = Math.floor(easeOut * target);
+      
+      if (currentStep >= steps) {
+        this[prop] = target;
+        clearInterval(timer);
+      }
+    }, stepTime);
+  }
+}
